877e70f0126c4cd781c698cec28d2361b8eb885c
Karl Xu <karl.yogi.xu@gmail.com>
2017-04-12 00:38:34 +0800

    first commit

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { render } from 'react-dom'
+import PropTypes from 'prop-types'
+import createReactClass from 'create-react-class'
+
+
+const App = function()
+{
+    return (
+        <div>test</div>
+    )
+}
+
+render(
+    <App></App>,
+    document.getElementById('root')
+)
